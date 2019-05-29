efa999589b14477cd137bf82754870a10d9f8287
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-05-29 18:20:32 +0300

    upd

--- send-email.php
+++ send-email.php
@@ -4,7 +4,7 @@ if($_POST){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = 'User: '.$name. ' Message: '.$_POST['message'];
-    $sendTo = 'valeriiderevshchykov@gmail.com';
+    $sendTo = 'contact@whiteparkc.capital';
 //send email
     mail($sendTo , "This is an email from: " .$email, $message);
 }
